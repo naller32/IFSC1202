818c91fe481ebc99d6bd094cf4ca6a0cd4b3ec52
naller32 <anall@ualr.edu>
2022-01-26 05:38:36 +0000

    01.02 Hello.py

---
+++ 01.02 Hello.py
@@ -0,0 +1,7 @@
+firstname = input("Please enter your first name: ")
+greeting = "Hello" + " " + firstname
+print(greeting)
+
+firstname = input("Please enter your first name: ")
+greeting = "Hello " + firstname
+print(greeting)
