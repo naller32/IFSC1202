d4f5de077791debeb928d0103d42ac8f2542339f
naller32 <anall@ualr.edu>
2022-01-26 04:51:56 +0000

    01.00.09 Inputting Data.py

---
+++ 01.00.09 Inputting Data.py
@@ -0,0 +1,5 @@
+firstname = input("Andrew")  # Input statement without a prompt
+print("You Entered: " + firstname)
+
+lastname = input("Nall")  # Input statement with a prompt
+print("You Entered: " + lastname)
