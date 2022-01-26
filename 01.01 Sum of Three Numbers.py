0928e65a99d65b243bf5e27c0b6f12f6f5eb4551
naller32 <anall@ualr.edu>
2022-01-26 05:25:20 +0000

    01.01 Sum of Three Numbers.py

--- 01.01 Sum of Three Numbers.py
+++ 01.01 Sum of Three Numbers.py
@@ -1,5 +1,8 @@
-x = 2
-y = 3
-z = 4
-print("")
-print(y*y)
+x = input("First Number: ")
+y = input("Second Number: ")
+z = input("Third Number: ")
+firstnumber = int(x)
+secondnumber = int(y)
+thirdnumber = int(z)
+Total = secondnumber * secondnumber
+print(Total)
